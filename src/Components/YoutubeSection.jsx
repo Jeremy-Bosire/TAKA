023a66f50487711a0b4802228c7b259cb1878461
Jeremy Bosire <bosirejeremy4@gmail.com>
2025-02-03 12:05:24 +0300

    Adding the main YouTube Video

--- src/Components/YoutubeSection.jsx
+++ src/Components/YoutubeSection.jsx
@@ -7,7 +7,7 @@ function YoutubeSection (){
         <iframe
           width="641.78"
           height="361"
-          src="https://www.youtube.com/embed/x9yop0nYR9g"
+          src="https://www.youtube.com/embed/xvK74yqldkY"
           title="YouTube video player"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
